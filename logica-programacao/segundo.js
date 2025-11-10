f6c1e227df027b68b5757d61685d06a591cf674d
Mikely09 <mikoliver09@gmail.com>
2025-11-10 16:01:09 -0300

    novo comentário

--- logica-programacao/segundo.js
+++ logica-programacao/segundo.js
@@ -1,4 +1,11 @@
-let numero1 = prompt ("Digite um numero");
-let numero2 = prompt ("Digite outro numero");
+let numero1 = Number(prompt("Digite um numero"));
+let numero2 = Number(prompt("Digite outro numero"));
 
-console.log (numero1 + numero2)
+//Soma
+console.log ( "Soma: " ,numero1 + numero2);
+//Sub
+console.log ( "Sub: " , numero1 - numero2);
+//Mult
+console.log ( " Mult:" + numero1 * numero2)
+//Div
+console.log ( "Div:" + numero1 / numero2)
