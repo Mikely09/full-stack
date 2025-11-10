1b90e1fc25482be0abf43a3d48fd8798b68c3626
Mikely09 <mikoliver09@gmail.com>
2025-11-10 16:27:45 -0300

    novo comentario

---
+++ logica-programacao/ano.js
@@ -0,0 +1,13 @@
+//Criar um programa, para verificar o ano de nascimento
+const anoAtual= 2025;
+
+/*
+Pegar o ano atual
+*/
+
+document.write( "EU nasci em: " + (anoAtual-16));
+
+//Diego,35 anos
+//Juliana,10 anos
+//joão, 17 anos
+  document.write( "Diego nasceu em: "+ (anoAtual-35));
---
+++ logica-programacao/copa.js
@@ -0,0 +1,9 @@
+//Descobir o ano da próxima Copa do Mundo
+const anoAtual=2025
+
+/*
+A copa do Mundo acontece a cada 4 anos
+A última foi em 2022
+*/
+
+document.write ("A proxima copa do mundo sera em:2026")
