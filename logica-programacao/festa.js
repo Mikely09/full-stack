f682dff49b7443db88823db8c8e2edf6d6bdc6be
Mikely09 <mikoliver09@gmail.com>
2025-11-12 14:59:18 -0300

    novo comentario

---
+++ logica-programacao/festa.js
@@ -0,0 +1,13 @@
+/*
+A pessoa é vai para festa
+se for maior que 18 anos
+*/
+let idade = 18;
+
+
+if ( idade < 18) {
+    console.log (" sinto muito")
+
+} else {
+    console.log (" Bem vindo! ")
+}
---
+++ logica-programacao/votar.js
@@ -0,0 +1,20 @@
+/*
+Enunciado:
+Crie um programa que peça a idade de uma pessoa e mostre
+uma mensagem dizendo se ela pode votar ou ainda não pode votar.
+*/
+let idade = 16;
+
+if (idade >= 16); {
+    console.log ("voce já pode votar!");
+
+} if (idade >=16 && idade <18) {
+alert ("Voto opsional");
+} else { 
+ if ("Voto obrigatorio") 
+
+}
+
+
+    console.log ("sinto muito, voce ainda nao pode votar!");
+    
