f85cd5bc3bbacf07ac0dfa86bb7545a612b45f1f
Mikely09 <mikoliver09@gmail.com>
2025-11-24 16:25:08 -0300

    adicionando um novo comentario

--- node-aula01/app.js
+++ node-aula01/app.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const app = express();
-const port = 300;
+const port = 3000;
+
+//Variável com os dados produtos
+const produtos =[
+    {id:1, nome: 'Produto A', preco: 100},
+    {id:2, nome: 'Produto B', preco: 150},
+    {id:2, nome: 'Produto B', preco: 200},
+];
 
 //Criar funçao
 //Rota principal
-app.get ('/', (req, res) => {
+app.get('/', (req, res) => {
  res.send('Bem vindo ao sevidor Node.js');
+});
+
+//Rota produtos
+app.get ('/produtos', (req,res) => {
+    res.send (produtos);
+});
+
+//Rota home
+app.get('/home', (req,res)=>{
+    console.log("olá:",__dirname);
+    res.sendFile(__dirname +'/public/home.html');
 });
 
 //Executando o servidor
 app.listen(port,() => {
-    console.log('Servindo rodando em http://localhost:${port}');
+    console.log(`Servindo rodando em http://localhost:${port}`);
 });
