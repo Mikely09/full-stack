07eb7c8d3bc1c52779c5edbe93cc48268125da54
Mikely09 <mikoliver09@gmail.com>
2025-12-08 16:19:36 -0300

    adicionando nova rota delete

--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -1,22 +1,43 @@
 import express from "express";
 import conexao from "../infra/conexao.js"
-const app = express ();
+const app = express();
 
-app.get ("/",(req,res) => {
-res.send ("Olá Copa do mundo!");
+app.get("/", (req, res) => {
+    res.send("Olá Copa do mundo!");
 });
 
 //Buscar todas as seleções
-app.get('/selecoes',(req,res)=>{
+app.get('/selecoes', (req, res) => {
     const sql = "select * from selecoes";
-    
-    conexao.query(sql, (erro,result)=>{
-res.json(result);
+
+    conexao.query(sql, (erro, result) => {
+        res.json(result);
     });
 
 });
 
+//Buscando por ID
+app.get('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "select * from selecoes where id=?;";
 
+    conexao.query(sql, id, (erro, result) => {
+        res.json(result[0]);
+    });
+
+});
+
+//Deletando registro
+app.delete ('/selecoes/:id', (req,res) => {
+const id = req.params.id;
+const sql = "delete from selecoes where id =?;";
+    
+conexao.query (sql,id,(erro,result) => {
+    res.json({mensagem : "Deletando com sucesso"});
+
+});
+
+});
 
 export default app;
 
